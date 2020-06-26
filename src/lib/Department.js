e1a6d0186c8d3df8d9cb51d85faf0db144cfe410
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-06-26 10:28:38 +0100

    Added relevant classes for db

---
+++ src/lib/Department.js
@@ -0,0 +1,12 @@
+class Department {
+  constructor(department_id, name) {
+    this.department_id = department_id;
+    this.name = name;
+  }
+  getDepartmentId() {
+    return this.department_id;
+  }
+  getName() {
+    return this.name;
+  }
+}
