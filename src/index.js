a8c9f7b069451e2e9d330c381882a1e334140625
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-07-08 10:49:02 +0100

    Implemented add employee, role, department functions

--- src/index.js
+++ src/index.js
@@ -187,6 +187,211 @@ const init = async () => {
 
     connection.query(query, onQuery);
   }
+
+  // View all employees by manager
+  if (selection === "employeesByManager") {
+    const queryManagers = `
+    SELECT employee.id, employee.first_name, employee.last_name FROM employee
+    INNER JOIN (SELECT DISTINCT(manager_id) FROM company_db.employee WHERE manager_id IS NOT NULL) as manager
+    on employee.id = manager.manager_id
+    `;
+
+    const onQuery = async (err, managers) => {
+      if (err) throw err;
+
+      const choices = managers.map((manager) => {
+        return {
+          name: `${manager.first_name} ${manager.last_name}`,
+          value: manager.id,
+          short: `${manager.first_name} ${manager.last_name}`,
+        };
+      });
+
+      const questions = [
+        {
+          message: "Select a manager:",
+          name: "managerId",
+          type: "list",
+          choices,
+        },
+      ];
+
+      const { managerId } = await inquirer.prompt(questions);
+
+      const queryEmployeesByRole = `
+      SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department
+      FROM employee LEFT JOIN role on employee.role_id = role.id
+      LEFT JOIN department on role.department_id = department.id
+      WHERE employee.manager_id = ${managerId}
+      `;
+
+      const onEmployeeQuery = (err, employees) => {
+        if (err) throw err;
+        console.table(employees);
+        init();
+      };
+
+      connection.query(queryEmployeesByRole, onEmployeeQuery);
+    };
+
+    connection.query(queryManagers, onQuery);
+  }
+
+  // Add a department
+  if (selection === "addDepartment") {
+    const questions = [
+      {
+        message: "What is the name of the department?",
+        name: "name",
+      },
+    ];
+
+    const { name } = await inquirer.prompt(questions);
+
+    const query = `INSERT INTO department (name) VALUES ("${name}") `;
+
+    const onQuery = (err) => {
+      if (err) throw err;
+      console.log("Successfully created a department");
+      init();
+    };
+
+    connection.query(query, onQuery);
+  }
+
+  // Add a role
+  if (selection === "addRole") {
+    const queryDepartments = "SELECT * FROM department";
+
+    const onQuery = async (err, departments) => {
+      if (err) throw err;
+
+      const choices = departments.map((department) => {
+        return {
+          name: department.name,
+          value: department.id,
+          short: department.name,
+        };
+      });
+
+      const questions = [
+        {
+          message: "What is the role title?",
+          name: "title",
+          type: "input",
+        },
+        {
+          message: "What is the salary?",
+          name: "salary",
+          type: "input",
+        },
+        {
+          message: "What department does the role belong to?",
+          name: "departmentId",
+          type: "list",
+          choices,
+        },
+      ];
+
+      const { title, salary, departmentId } = await inquirer.prompt(questions);
+
+      const addRoleQuery = `INSERT INTO role (title, salary, department_id) VALUES ("${title}", ${parseFloat(
+        salary
+      )}, ${parseInt(departmentId)})`;
+
+      const onAddRole = (err) => {
+        if (err) throw err;
+        console.log("Successfully added role to DB");
+        init();
+      };
+
+      connection.query(addRoleQuery, onAddRole);
+    };
+
+    connection.query(queryDepartments, onQuery);
+  }
+
+  // Add an employee
+  if (selection === "addEmployee") {
+    const queryRoles = "SELECT * FROM role";
+    const queryManagers = `
+    SELECT employee.id, employee.first_name, employee.last_name FROM employee
+    INNER JOIN (SELECT DISTINCT(manager_id) FROM company_db.employee WHERE manager_id IS NOT NULL) as manager
+    on employee.id = manager.manager_id
+    `;
+
+    const onQuery = async (err, rows) => {
+      if (err) throw err;
+      const [roles, managers] = rows;
+
+      const roleChoices = roles.map((role) => {
+        return {
+          name: role.title,
+          value: role.id,
+          short: role.title,
+        };
+      });
+
+      const managerChoices = managers.map((manager) => {
+        return {
+          name: `${manager.first_name} ${manager.last_name}`,
+          value: manager.id,
+          short: `${manager.first_name} ${manager.last_name}`,
+        };
+      });
+
+      const questions = [
+        {
+          message: "What is your first name?",
+          name: "firstName",
+          type: "input",
+        },
+        {
+          message: "What is your last name?",
+          name: "lastName",
+          type: "input",
+        },
+        {
+          message: "Select a role:",
+          name: "roleId",
+          type: "list",
+          choices: roleChoices,
+        },
+        {
+          message: "Do you want to select a manager?",
+          name: "manager",
+          type: "confirm",
+        },
+        {
+          message: "Select manager:",
+          name: "managerId",
+          type: "list",
+          choices: managerChoices,
+          when: (answers) => {
+            return answers.manager;
+          },
+        },
+      ];
+
+      const answers = await inquirer.prompt(questions);
+
+      const addEmployeeQuery = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${
+        answers.firstName
+      }", "${answers.lastName}", ${answers.roleId}, ${
+        answers.managerId || null
+      })`;
+
+      const onEmployeeAddQuery = (err) => {
+        if (err) throw err;
+        console.log("Successfully added employee to DB");
+        init();
+      };
+
+      connection.query(addEmployeeQuery, onEmployeeAddQuery);
+    };
+
+    connection.query(`${queryRoles}; ${queryManagers}`, onQuery);
+  }
 };
 
 const onConnect = () => {
